649fb72d49523bc91132bb953c0cd42d8069a1a8
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-11-20 10:46:49 -0300

    Get method

---
+++ src/storage/itemsStorage.ts
@@ -0,0 +1,24 @@
+import { FilterStatus } from "@/types/FilterStatus";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const ITEM_STORAGE_KEY = "@comprar:items";
+
+export type ItemStorage = {
+  id: string;
+  status: FilterStatus;
+  description: string;
+};
+
+async function get(): Promise<ItemStorage[]> {
+  try {
+    const storedItems = await AsyncStorage.getItem(ITEM_STORAGE_KEY);
+
+    return storedItems ? JSON.parse(storedItems) : [];
+  } catch (error) {
+    throw new Error("GET_ITEMS: " + error);
+  }
+}
+
+const itemsStorage = {
+  get,
+};
